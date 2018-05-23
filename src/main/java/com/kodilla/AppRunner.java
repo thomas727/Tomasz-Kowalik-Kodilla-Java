9273036149bf27135a186471e6713a7b0f33d7d3
thomas727 <kowalik.tl@gmail.com>
2018-05-23 23:36:51 +0200

    first app

--- src/main/java/com/kodilla/AppRunner.java
+++ src/main/java/com/kodilla/AppRunner.java
@@ -1,4 +1,13 @@
 package com.kodilla;
 
 public class AppRunner {
+    public static void main(String ...args){
+        Calculator calculator= new Calculator();
+        int result=calculator.addition(2,3);
+        if (result==8){
+            System.out.print("success");
+        }else {
+            System.out.print("fail");
+        }
+    }
 }
--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,7 @@
 package com.kodilla;
 
 public class Calculator {
+    public int addition (int x,int y){
+        return x+y;
+    }
 }
